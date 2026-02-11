c3620bdfa2aa6df540d91021be354ff54c131abb
Ikigami Sama <ikigamidevs.15@gmail.com>
2026-02-11 18:57:38 +0800

    Update

--- main.py
+++ main.py
@@ -12,10 +12,15 @@ st.markdown(
         div[data-testid="stMetric"] {
             border-left: 0.5rem solid #6366f1 !important;
             box-shadow: 0 0.15rem 1.75rem 0 rgba(58, 59, 69, 0.15) !important;
-            padding: 5% 5% 5% 10% !important;
+            padding: 0 0 0 2% !important;
             border-radius: 12px !important;
             background-color: transparent !important;
         }
+        div[data-testid="stMetric"] label{
+            margin-bottom: 5px;
+         }
+
+        
     </style>
     """,
     unsafe_allow_html=True
